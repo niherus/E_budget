644797d2a44e0128fa291c8c9f21764ee30c1d36
Niherus <tnb6@yandex.ru>
2018-07-31 17:50:25 +0300

    Database logic started

--- database_logic.py
+++ database_logic.py
@@ -1,1 +1,21 @@
-import sqlite3
+from PyQt5 import QtWidgets, QtSql
+import sys
+
+import os
+
+class DatabaseError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+con1 = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+filePath = './hope.db'
+if  os.path.isfile(filePath):
+    print('Ok')
+    con1.setDatabaseName(filePath)
+    con1.open()
+else:
+    raise DatabaseError("Database doesn't exist")
+print(con1.size())
